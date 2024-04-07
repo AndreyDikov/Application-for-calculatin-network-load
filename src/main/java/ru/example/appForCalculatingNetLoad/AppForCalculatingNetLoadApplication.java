a97c2875e5f826dd83f6d16059fa170b1b2f8084
Andrey <dikovandrey856@gmail.com>
2024-04-07 15:52:32 +0500

    Initial commit

---
+++ src/main/java/ru/example/appForCalculatingNetLoad/AppForCalculatingNetLoadApplication.java
@@ -0,0 +1,13 @@
+package ru.example.appForCalculatingNetLoad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppForCalculatingNetLoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppForCalculatingNetLoadApplication.class, args);
+	}
+
+}
